2c33f20d89c80caa145b6811039380d8c149e550
Ashley Nguyen <ashleygnguyen@yahoo.com>
2018-09-18 22:03:56 -0500

    edit

---
+++ src/components/UploadPic/UploadPic.js
@@ -0,0 +1,13 @@
+// import React, { Component } from "react";
+
+// class UploadPic extends Component {
+//   fileSelectedHandler = event => {
+//       console.log(event);
+//   };
+// }
+
+// render() {
+// return (
+//     <div className="photoupload"
+// )
+// }
